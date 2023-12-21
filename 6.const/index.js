a70369073afb7ebee040050537e0216f4512d734
unknown <meggiegambill2006@gmail.com>
2023-12-21 14:46:01 -0500

    const complete\

---
+++ 6.const/index.js
@@ -0,0 +1,14 @@
+// const = a variable that can't be changed
+
+const PI = 3.14159;
+let radius;
+let circumference;
+
+radius = window.prompt("Enter the radius of the sircle");
+radius = Number(radius);
+
+pi = 420.69;
+
+circumference = 2 * PI * radius;
+
+console.log("The circumference is", circumference);
