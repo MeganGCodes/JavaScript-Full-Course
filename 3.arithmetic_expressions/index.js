aa123456f0630e64a2fd7e3e0efdb95175c763e9
unknown <meggiegambill2006@gmail.com>
2023-12-21 13:40:21 -0500

    arithmetic expressions complete

---
+++ 3.arithmetic_expressions/index.js
@@ -0,0 +1,35 @@
+/*
+    arithmetic expression is a combination of...
+    operands (values, variables, etc.)
+    operators (+ - * / %)
+    that can be evaluated to a value
+    ex. y = x + 5;
+*/
+
+let students = 20;
+
+//students = students + 1; will add 1
+//students = students - 1; will take 1 away
+//students = students * 2; will add 20 more
+//students = students / 2; will take away 10
+//let extraStudents = students %  3; shows if its a even or odd number
+
+//students += 1; shorter way of doing it
+//students -= 1;
+//students *= 2;
+//students /= 2;
+//console.log(students);
+/*
+    operator precedence
+    1. parenthesis ()
+    2. exponents
+    3. mutiplication & division
+    addition & subtraction
+*/
+
+let result = (1 + 2 ) * (3 + 4);
+
+console.log(result);
+
+
+
